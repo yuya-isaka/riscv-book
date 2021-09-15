24b18f12b276eac9e727558c60d10a6cdf04b7d0
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-09-15 16:18:49 +0900

    add lw instruction

---
+++ src/main/scala/03_lw/Top.scala
@@ -0,0 +1,22 @@
+package lw
+
+import chisel3._
+import chisel3.util._
+
+class Top extends Module {
+	// 信号を生成
+	val io = IO(new Bundle {
+		val exit = Output(Bool())
+	})
+
+	// CoreクラスとMemoryクラスをnewでインスタンス化，Moduleでハードウェア化
+	val core		= Module(new Core())
+	val memory 		= Module(new Memory())
+
+	// coreのioとmemoryのioはImemPortIoを反転した関係にあるので，"<>"で一括接続
+	core.io.imem <> memory.io.imem
+	core.io.dmem <> memory.io.dmem
+
+	// coreの出力をtopの出力に配線
+	io.exit := core.io.exit
+}
---
+++ src/test/scala/LwTest.scala
@@ -0,0 +1,19 @@
+package lw
+
+import chisel3._
+
+// ChiselTestを利用するために必要なpackage
+import org.scalatest._
+import chiseltest._
+
+// ２つのトレイトをテストクラスに継承
+class HexTest extends FlatSpec with ChiselScalatestTester {
+	"mycpu" should "work through hex" in {
+		test(new Top) { c =>
+		// このブロックでテストを記述（変数cはTopクラスのインスタンス
+		while (!c.io.exit.peek().litToBoolean) {
+			c.clock.step(1)
+		}
+		}
+	}
+}
