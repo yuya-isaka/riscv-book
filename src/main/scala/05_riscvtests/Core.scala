91a83b4a4884ecd92ff561ae4169315568fe7ba1
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-09-20 14:41:41 +0900

    power up decoder

--- src/main/scala/05_riscvtests/Core.scala
+++ src/main/scala/05_riscvtests/Core.scala
@@ -6,6 +6,7 @@ import common.Consts._
 import common.Instructions._
 
 // instとregfileが重要
+// デコードの強化で，instをEX以降のステージに受け渡す必要がなくなった
 
 // 5つのステージ
 class Core extends Module {
@@ -51,20 +52,57 @@ class Core extends Module {
 	val imm_s = Cat(inst(31, 25), inst(11, 7))
 	val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s) // 符号拡張
 
+	val csignals = ListLookup(inst,
+			List(ALU_X, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+		Array(
+			LW -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_MEM),
+			SW -> List(ALU_ADD, OP1_RS1, OP2_IMS, MEN_S, REN_X, WB_X),
+			ADD -> List(ALU_ADD, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			ADDI -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			SUB -> List(ALU_SUB, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			AND -> List(ALU_AND, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			OR -> List(ALU_OR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			XOR -> List(ALU_XOR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			ANDI -> List(ALU_AND, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			ORI -> List(ALU_OR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			XORI -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+		)
+	)
+
+	val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rf_wen :: wb_sel :: Nil = csignals
+
+	val op1_data = MuxCase(0.U(WORD_LEN.W), Seq(
+		(op1_sel === OP1_RS1) -> rs1_data
+	))
+
+	val op2_data = MuxCase(0.U(WORD_LEN.W), Seq(
+		(op2_sel === OP2_RS2) -> rs2_data,
+		(op2_sel === OP2_IMI) -> imm_i_sext,
+		(op2_sel === OP2_IMS) -> imm_s_sext,
+	))
+
 	// EX -----------------------------------------------------
 
 	// 演算結果格納
+	// val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
+	// 	(inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext),
+	// 	(inst === SW) -> (rs1_data + imm_s_sext),
+	// 	(inst === ADD) -> (rs1_data + rs2_data),
+	// 	(inst === SUB) -> (rs1_data - rs2_data),
+	// 	(inst === AND) -> (rs1_data & rs2_data),
+	// 	(inst === OR) -> (rs1_data | rs2_data),
+	// 	(inst === XOR) -> (rs1_data ^ rs2_data),
+	// 	(inst === ANDI) -> (rs1_data & imm_i_sext),
+	// 	(inst === ORI) -> (rs1_data | imm_i_sext),
+	// 	(inst === XORI) -> (rs1_data ^ imm_i_sext)
+	// ))
+
 	val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
-		(inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext),
-		(inst === SW) -> (rs1_data + imm_s_sext),
-		(inst === ADD) -> (rs1_data + rs2_data),
-		(inst === SUB) -> (rs1_data - rs2_data),
-		(inst === AND) -> (rs1_data & rs2_data),
-		(inst === OR) -> (rs1_data | rs2_data),
-		(inst === XOR) -> (rs1_data ^ rs2_data),
-		(inst === ANDI) -> (rs1_data & imm_i_sext),
-		(inst === ORI) -> (rs1_data | imm_i_sext),
-		(inst === XORI) -> (rs1_data ^ imm_i_sext)
+		(exe_fun === ALU_ADD) -> (op1_data + op2_data),
+		(exe_fun === ALU_SUB) -> (op1_data - op2_data),
+		(exe_fun === ALU_AND) -> (op1_data & op2_data),
+		(exe_fun === ALU_OR) -> (op1_data | op2_data),
+		(exe_fun === ALU_XOR) -> (op1_data ^ op2_data),
 	))
 
 	// MEM access -----------------------------------------------------
@@ -73,19 +111,28 @@ class Core extends Module {
 	io.dmem.addr := alu_out
 
 	// ストアしたいデータをメモリへ（SW）
-	io.dmem.wen := (inst === SW)
+	// io.dmem.wen := (inst === SW)
+	io.dmem.wen := mem_wen // instの再デコード処理が不要に
 	io.dmem.wdata := rs2_data
 
 	// WB -----------------------------------------------------
 
 	// LW結果を受け取り(LW命令でWB使うんだな)
 	// val wb_data = io.dmem.rdata
+	// val wb_data = MuxCase(alu_out, Seq(
+	// 	(inst === LW) -> io.dmem.rdata
+	// ))
+
+	// デフォルトは演算結果をWB, LW命令の時はメモリからの出力をWB
 	val wb_data = MuxCase(alu_out, Seq(
-		(inst === LW) -> io.dmem.rdata
+		(wb_sel === WB_MEM) -> io.dmem.rdata
 	))
 
 	// WBするものだけ記述
-	when(inst === LW || inst === ADD || inst === ADDI || inst === SUB || inst === AND || inst === OR || inst === XOR || inst === ANDI || inst === ORI || inst === XORI) {
+	// when(inst === LW || inst === ADD || inst === ADDI || inst === SUB || inst === AND || inst === OR || inst === XOR || inst === ANDI || inst === ORI || inst === XORI) {
+	// 	regfile(wb_addr) := wb_data
+	// }
+	when(rf_wen === REN_S) {
 		regfile(wb_addr) := wb_data
 	}
 
