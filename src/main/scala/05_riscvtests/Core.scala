f00874b3945ecb66cfcac7a37b157db14e9b0b3e
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-09-21 20:34:11 +0900

    SLL, SLT, BEQ

--- src/main/scala/05_riscvtests/Core.scala
+++ src/main/scala/05_riscvtests/Core.scala
@@ -27,7 +27,17 @@ class Core extends Module {
 
 	// プログラムカウンタ
 	val pc_reg = RegInit(START_ADDR) // 単体のレジスタはこれで作る
-	pc_reg := pc_reg + 4.U(WORD_LEN.W)
+	// pc_reg := pc_reg + 4.U(WORD_LEN.W)
+	val pc_plus4 = pc_reg + 4.U(WORD_LEN.W)
+
+	// Wireで宣言のみ
+	val br_flg = Wire(Bool())
+	val br_target = Wire(UInt(WORD_LEN.W))
+
+	val pc_next = MuxCase(pc_plus4, Seq(
+		br_flg -> br_target
+	))
+	pc_reg := pc_next
 
 	// メモリから命令を受け取る
 	io.imem.addr := pc_reg
@@ -52,20 +62,41 @@ class Core extends Module {
 	val imm_s = Cat(inst(31, 25), inst(11, 7))
 	val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s) // 符号拡張
 
+	// 分岐命令の即値(B形式)
+	val imm_b = Cat(inst(31), inst(7), inst(30, 25), inst(11, 8))
+	val imm_b_sext = Cat(Fill(19, imm_b(11)), imm_b, 0.U(1.U)) // 最下位ビットを0にセットすることで必ず２の倍数
+
+	// 演算内容，　オペランド１，　オペランド２，　メモリストアか否か，　WBするか否か，　WBのデータはどれ？
 	val csignals = ListLookup(inst,
 			List(ALU_X, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
 		Array(
-			LW -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_MEM),
-			SW -> List(ALU_ADD, OP1_RS1, OP2_IMS, MEN_S, REN_X, WB_X),
-			ADD -> List(ALU_ADD, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
-			ADDI -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
-			SUB -> List(ALU_SUB, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
-			AND -> List(ALU_AND, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
-			OR -> List(ALU_OR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
-			XOR -> List(ALU_XOR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
-			ANDI -> List(ALU_AND, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
-			ORI -> List(ALU_OR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
-			XORI -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			LW 		-> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_MEM),
+			SW 		-> List(ALU_ADD, OP1_RS1, OP2_IMS, MEN_S, REN_X, WB_X),
+			ADD 	-> List(ALU_ADD, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			ADDI 	-> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			SUB 	-> List(ALU_SUB, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			AND 	-> List(ALU_AND, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			OR 		-> List(ALU_OR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			XOR 	-> List(ALU_XOR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			ANDI 	-> List(ALU_AND, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			ORI 	-> List(ALU_OR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			XORI 	-> List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			SLL 	-> List(ALU_SLL, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			SRL 	-> List(ALU_SRL, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			SRA 	-> List(ALU_SRA, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			SLLI 	-> List(ALU_SLL, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			SRLI 	-> List(ALU_SRL, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			SRAI 	-> List(ALU_SRA, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			SLT 	-> List(ALU_SLT, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			SLTU 	-> List(ALU_SLTU, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+			SLTI 	-> List(ALU_SLT, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			SLTUI 	-> List(ALU_SLTU, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+			BEQ 	-> List(ALU_BEQ, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+			BNE 	-> List(ALU_BNE, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+			BLT 	-> List(ALU_BLT, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+			BGE 	-> List(ALU_BGE, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+			BLTU 	-> List(ALU_BLTU, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+			BGEU 	-> List(ALU_BGEU, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
 		)
 	)
 
@@ -97,13 +128,32 @@ class Core extends Module {
 	// 	(inst === XORI) -> (rs1_data ^ imm_i_sext)
 	// ))
 
+	// UInt型が入る
 	val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
-		(exe_fun === ALU_ADD) -> (op1_data + op2_data),
-		(exe_fun === ALU_SUB) -> (op1_data - op2_data),
-		(exe_fun === ALU_AND) -> (op1_data & op2_data),
-		(exe_fun === ALU_OR) -> (op1_data | op2_data),
-		(exe_fun === ALU_XOR) -> (op1_data ^ op2_data),
+		(exe_fun === ALU_ADD) 	-> (op1_data + op2_data),
+		(exe_fun === ALU_SUB) 	-> (op1_data - op2_data),
+		(exe_fun === ALU_AND) 	-> (op1_data & op2_data),
+		(exe_fun === ALU_OR) 	-> (op1_data | op2_data),
+		(exe_fun === ALU_XOR) 	-> (op1_data ^ op2_data),
+		(exe_fun === ALU_SLL) 	-> (op1_data << op2_data(4, 0))(31, 0), // 溢れた桁は捨てる(オーバーフローは捨てる)
+		(exe_fun === ALU_SRL) 	-> (op1_data >> op2_data(4, 0)).asUInt(),
+		(exe_fun === ALU_SRA) 	-> (op1_data.asSInt() >> op2_data(4, 0)).asUInt(),
+		(exe_fun === ALU_SLT) 	-> (op1_data.asSInt() < op2_data.asSInt()).asUInt(),
+		(exe_fun === ALU_SLTU) 	-> (op1_data < op2_data).asUInt(),
 	))
+
+	// 分岐するか否か
+	br_flg := MuxCase(false.B, Seq(
+		(exe_fun === BR_BEQ)	-> (op1_data === op2_data),
+		(exe_fun === BR_BNE)	-> !(op1_data === op2_data),
+		(exe_fun === BR_BLT)	-> (op1_data.asSInt() < op2_data.asSInt()),
+		(exe_fun === BR_BGE)	-> !(op1_data.asSInt() < op2_data.asSInt()),
+		(exe_fun === BR_BLTU)	-> (op1_data < op2_data),
+		(exe_fun === BR_BGEU)	-> !(op1_data < op2_data),
+	))
+
+	// 分岐先のメモリアドレス
+	br_target := pc_reg + imm_b_sext
 
 	// MEM access -----------------------------------------------------
 
