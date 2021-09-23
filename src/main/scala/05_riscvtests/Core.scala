e46158ef74543d0015f347c5e2a304ef0c60846a
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-09-23 16:36:56 +0900

    LUI, AUIPC

--- src/main/scala/05_riscvtests/Core.scala
+++ src/main/scala/05_riscvtests/Core.scala
@@ -74,6 +74,10 @@ class Core extends Module {
 	val imm_j = Cat(inst(31), inst(19, 12), inst(20), inst(30, 21))
 	val imm_j_sext = Cat(Fill(11, imm_j(19)), imm_j, 0.U(1.U))
 
+	// 即値ロード命令の即値(U形式)
+	val imm_u = inst(31, 12)
+	val imm_u_shifted = Cat(imm_u, Fill(12, 0.U)) // 12bit左シフト
+
 	// 演算内容，　オペランド１，　オペランド２，　メモリストアか否か，　WBするか否か，　WBのデータはどれ？
 	val csignals = ListLookup(inst,
 			List(ALU_X, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
@@ -107,6 +111,8 @@ class Core extends Module {
 			BGEU 	-> List(ALU_BGEU, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
 			JAL 	-> List(ALU_ADD, OP1_PC, OP2_IMJ, MEN_X, REN_S, WB_PC),
 			JALR 	-> List(ALU_JALR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_PC),
+			LUI		-> List(ALU_ADD, OP1_X, OP2_IMU, MEN_X, REN_S, WB_ALU),
+			AUIPC	-> List(ALU_ADD, OP1_PC, OP2_IMU, MEN_X, REN_S, WB_ALU)
 		)
 	)
 
@@ -121,7 +127,8 @@ class Core extends Module {
 		(op2_sel === OP2_RS2) -> rs2_data,
 		(op2_sel === OP2_IMI) -> imm_i_sext,
 		(op2_sel === OP2_IMS) -> imm_s_sext,
-		(op2_sel === OP2_IMJ) -> imm_j_sext
+		(op2_sel === OP2_IMJ) -> imm_j_sext,
+		(op2_sel === OP2_IMU) -> imm_u_shifted
 	))
 
 	// EX -----------------------------------------------------
