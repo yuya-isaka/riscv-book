dafcca27a86d84656207376afc4674a060b3d1bf
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-10-04 00:17:18 +0900

    add ctest.hex etc

---
+++ src/test/scala/CTest.scala
@@ -0,0 +1,28 @@
+/*
+テストの流れ
+・Chiselテストコードファイルを作成
+・テスト用のhexファイルを作成
+・hexファイルをMemoryクラスでロード
+・Coreクラスでデバッグ信号を出力
+・sbtを利用してScalaコードをコンパイルした後，テストを実行
+*/
+
+package ctest
+
+import chisel3._
+
+// ChiselTestを利用するために必要なpackage
+import org.scalatest._
+import chiseltest._
+
+// ２つのトレイトをテストクラスに継承
+class HexTest extends FlatSpec with ChiselScalatestTester {
+	"mycpu" should "work through hex" in {
+		test(new Top) { c =>
+		// このブロックでテストを記述（変数cはTopクラスのインスタンス
+		while (!c.io.exit.peek().litToBoolean) {
+			c.clock.step(1)
+		}
+		}
+	}
+}
